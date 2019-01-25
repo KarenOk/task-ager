c1bedce5d1024c06206007feb9ef6be4aa40cccc
Karen Okonkwo <karenokonkwo29@gmail.com>
2019-01-25 01:14:01 +0100

    feature: welcome page displays before home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import React, { Component } from 'react';
-import logo from './images/logo4.svg';
-import './App.css';
+import React from "react";
+import logo from "./images/logo4.svg";
+import Home from "./components/Home/Home"
+import "./App.css";
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ready: false
+    }
+
+  }
+
+  handleHomeMount = (ready) => {
+    setTimeout(() => {
+      this.setState({
+        ready: true
+      });
+    }, 1000);
+  }
+
+  homePage = () => {
+    return (
+      <div className="home-container">
+        <Home handleMount={this.handleHomeMount} />
+      </div>
+    );
+  }
+
+  welcomePage = () => {
+    return (
+        <div className="welcome-page">
+          <img
+            src={logo}
+            className="logo"
+            alt="logo"
+            onAnimationEnd={this.handleHomeMount}
+          />
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="app">
-        <div className="logo-container">
-          <img src={logo} className="logo" alt="logo" />
-        </div>
+        {this.state.ready ? <this.homePage /> : <this.welcomePage />}
       </div>
     );
   }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div className="home">
+                Hello there
+            </div>
+        );
+    }
+}
+
+export default App;
