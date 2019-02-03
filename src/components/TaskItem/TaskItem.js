bfbfac365bba1825ada28fe7bed71ade35e7accf
Karen Okonkwo <karenokonkwo29@gmail.com>
2019-02-03 23:20:19 +0100

    feat:implemented edit and delete for each task. overlay implemented with css instead

--- src/components/TaskItem/TaskItem.js
+++ src/components/TaskItem/TaskItem.js
@@ -5,45 +5,58 @@ class TaskItem extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            showOverlay: false
+            editing: false
         }
+    }
+
+    startEdit = () => {
+        this.setState({
+            editing: true
+        });
+    }
+
+    stopEdit = (e) => {
+        if (e && e.keyCode !== 13) return;
+
+        this.setState({
+            editing: false
+        })
     }
 
     handleCheck = (e) => {
         this.props.handleItemCheck(e.target.checked);
     }
 
-    showOverlay = () => {
-        this.setState({
-            showOverlay: true
-        })
+    handleDelete = (e) => {
+        this.props.handleDeleteItem();
     }
 
-    removeOverlay = () => {
-        this.setState({
-            showOverlay: false
-        })
+    handleEdit = (e) => {
+        this.props.handleEditItem(e.target.value);
+
     }
 
     overlay = () => {
         return (
-            <div className="buttons">
+            <div className={`buttons ${this.state.editing ? "hidden" : ""}`}>
                 <input
                     type="checkbox"
                     name="select-all"
                     checked={this.props.item.done}
-                    onChange={this.props.handleItemCheck}
+                    onChange={this.handleCheck}
                     className="select-all"
                 />
                 <img
                     src={require("../../images/edit.png")}
                     className="icon"
                     alt="Edit"
+                    onClick={this.startEdit}
                 />
                 <img
                     src={require("../../images/delete.png")}
                     className="icon"
                     alt="Delete"
+                    onClick={this.handleDelete}
                 />
                 <img
                     src={require("../../images/move-icon.png")}
@@ -56,18 +69,24 @@ class TaskItem extends React.Component {
 
     render() {
         return (
-            <div 
-                className="task-item" 
-                onMouseEnter={this.showOverlay}
-                onMouseLeave={this.removeOverlay}
-            >
-                <p className={this.props.item.done ? "done" : ""}>
-                    {this.props.item.note}
-                </p>
-
+            <div
+                className="task-item">
+                {
+                    this.state.editing === false ?
+                        <p className={this.props.item.done ? "done" : ""}>
+                            {this.props.item.note}
+                        </p>
+                        : <textarea
+                            className="edit-area"
+                            value={this.props.item.note}
+                            onChange={this.handleEdit}
+                            onBlur={this.stopEdit}
+                            onKeyUp={this.stopEdit}
+                        />
+                }
                 <span> Due Date: {this.props.item.dueDate} </span>
-
-                {this.state.showOverlay ? <this.overlay /> : <div />}
+                
+                <this.overlay />
             </div>
         );
     }
