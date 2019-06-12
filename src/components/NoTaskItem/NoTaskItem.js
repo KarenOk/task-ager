a0c3d65b1f13d73a6bf714c885b09ede206155dc
karenok <karenokonkwo29@gmail.com>
2019-06-12 21:31:43 +0000

    Included header for task with no notes

--- src/components/NoTaskItem/NoTaskItem.js
+++ src/components/NoTaskItem/NoTaskItem.js
@@ -1,26 +1,44 @@
 import React from "react";
 import "./NoTaskItem.css";
+import TaskHeader from "../TaskHeader/TaskHeader";
 
 class NoTaskItem extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {};
     }
 
+    closeMenu = (e) => {
+        if (e.target.className !== "task-header") {
+            this.props.closeMenu();
+        }
+
+    }
+
     render() {
         return (
-            <div className="no-task-item">
-                <img
-                    src={require("../../images/question-class-note-symbol.svg")}
-                    className="logo"
-                    alt="logo"
-                />
-                <p> There is nothing here yet. </p>
-                <button
-                    onClick={this.props.toggleAddNote}>
-                    Add New Note
-                </button>
+            <div className="no-task-item" onClick={this.closeMenu}>
+                <div>
+                    <TaskHeader
+                        taskName={this.props.taskName}
+                        toggleAddNote={this.props.toggleAddNote}
+                        toggleDeleteTask={this.props.toggleDeleteTask}
+                        onMenuClick={this.props.onMenuClick}
+                        noNote={true}
+                    />
+                </div>
+
+                <div className="content">
+                    <img
+                        src={require("../../images/question-class-note-symbol.svg")}
+                        className="logo"
+                        alt="logo"
+                    />
+                    <p> There is nothing here yet. </p>
+
+                </div>
+
             </div>
         );
     }
