2c42808f4f270b5d91b3a72f19fe8218d7c1e0e4
Karen Okonkwo <karenokonkwo29@gmail.com>
2020-10-20 15:00:14 +0100

    Show service worker status to user

--- src/App.js
+++ src/App.js
@@ -1,59 +1,53 @@
 import React from "react";
-import { Switch, Route,  Redirect} from "react-router-dom";
+import "./App.css";
+import { Switch, Route, Redirect } from "react-router-dom";
 import logo from "./images/logo4.svg";
 import Main from "./components/Main/Main";
 import storage from "./services/storage";
-import "./App.css";
+import { ToastsContainer, ToastsStore } from "react-toasts";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      redirect: false
-    }
+		this.state = {
+			redirect: false
+		};
+	}
 
-  }
+	componentWillMount() {
+		storage.init();
+	}
 
-  componentWillMount(){
-    storage.init();
-  }
+	handleMainMount = () => {
+		setTimeout(() => {
+			this.setState({
+				redirect: true
+			});
+		}, 1000);
+	};
 
-  handleMainMount = () => {
-    setTimeout(() => {
-      this.setState({
-        redirect: true
-      });
-    }, 1000);
-  }
+	welcomePage = () => {
+		return (
+			<div className="welcome-page">
+				<img src={logo} className="logo" alt="logo" onAnimationEnd={this.handleMainMount} />
+			</div>
+		);
+	};
 
-  welcomePage = () => {
-    return (
-      <div className="welcome-page">
-        <img
-          src={logo}
-          className="logo"
-          alt="logo"
-          onAnimationEnd={this.handleMainMount}
-        />
-      </div>
-    );
-  }
-
-  render() {
-    return (
-      <div className="app">
-
-        <Switch>
-          <Route exact path="/" component={this.welcomePage} />
-          <Route path="/tasks" component={Main} />
-          <Route render={() => <Redirect to="/" />} />
-        </Switch>
-
-        {this.state.redirect ? <Redirect to="/tasks/all" /> : <div />}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="app">
+				<ToastsContainer store={ToastsStore} position="top_center" />
+				<Switch>
+					<Route exact path="/" component={this.welcomePage} />
+					<Route path="/tasks" component={Main} />
+					<Route render={() => <Redirect to="/" />} />
+				</Switch>
+				{this.state.redirect ? <Redirect to="/tasks/all" /> : <div />}
+			</div>
+		);
+	}
 }
 
 export default App;
