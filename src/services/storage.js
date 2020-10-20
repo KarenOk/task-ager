1b74ad6b0d22e6460c9082f00e21ea94c3172097
Karen Okonkwo <karenokonkwo29@gmail.com>
2020-10-20 11:41:58 +0100

    Fix: invalid date

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,72 +1,71 @@
 const storageController = {
+	getTaskNames: function () {
+		return JSON.parse(localStorage.taskNames);
+	},
 
-    getTaskNames: function () {
-        return JSON.parse(localStorage.taskNames);
-    },
+	setTaskNames: function (taskNames) {
+		localStorage.setItem("taskNames", JSON.stringify(taskNames));
+	},
 
-    setTaskNames: function (taskNames) {
-        localStorage.setItem("taskNames", JSON.stringify(taskNames));
-    },
+	getTasks: function () {
+		return JSON.parse(localStorage.tasks);
+	},
 
-    getTasks: function () {
-        return JSON.parse(localStorage.tasks);
-    },
+	setTasks: function (tasks) {
+		localStorage.setItem("tasks", JSON.stringify(tasks));
+	},
 
-    setTasks: function (tasks) {
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-    },
+	init: function () {
+		if (localStorage.taskNames && localStorage.tasks) return;
 
-    init: function () {
+		let tasks, taskNames;
 
-        if (localStorage.taskNames && localStorage.tasks) return;
+		if (!localStorage.taskNames) {
+			taskNames = ["Groceries", "School"];
+		}
 
-        let tasks, taskNames;
+		if (!localStorage.tasks) {
+			tasks = {
+				school: {
+					title: "School",
+					notes: [
+						{
+							id: 1,
+							note: "Note 1",
+							dueDate: new Date(),
+							done: false
+						},
+						{
+							id: 2,
+							note: "Note 2",
+							dueDate: new Date(),
+							done: true
+						}
+					]
+				},
+				groceries: {
+					title: "Groceries",
+					notes: [
+						{
+							id: 1,
+							note: "Grocery 1",
+							dueDate: new Date(),
+							done: true
+						},
+						{
+							id: 2,
+							note: "Go and find something that'll fill up your stomach so you'll be healthy",
+							dueDate: new Date(),
+							done: false
+						}
+					]
+				}
+			};
+		}
 
-        if (!localStorage.taskNames) {
-            taskNames = ["Groceries", "School"];
-        }
-
-        if (!localStorage.tasks) {
-            tasks = {
-                "school": {
-                    title: "School",
-                    notes: [
-                        {
-                            id: 1,
-                            note: "Note 1",
-                            dueDate: (new Date()).toLocaleString(),
-                            done: false
-                        }, {
-                            id: 2,
-                            note: "Note 2",
-                            dueDate: (new Date()).toLocaleString(),
-                            done: true
-                        }
-
-                    ]
-                },
-                "groceries": {
-                    title: "Groceries",
-                    notes: [
-                        {
-                            id: 1,
-                            note: "Grocery 1",
-                            dueDate: (new Date()).toLocaleString(),
-                            done: true
-                        }, {
-                            id: 2,
-                            note: "Go and find something that'll fill up your stomach so you'll be healthy",
-                            dueDate: (new Date()).toLocaleString(),
-                            done: false
-                        }
-                    ]
-                }
-            }
-        }
-
-        this.setTasks(tasks);
-        this.setTaskNames(taskNames);
-    },
+		this.setTasks(tasks);
+		this.setTaskNames(taskNames);
+	}
 };
 
-export default storageController; 
+export default storageController;
